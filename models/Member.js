f72337f68f3e6af8734961314bd1553eb9af2883
Patryk <patryk.iwasieczko@gmail.com>
2020-10-02 12:26:47 +0200

    Add member model

---
+++ models/Member.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+
+const memberSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Proszę podać adres e-mail"],
+        unique: true,
+        validate: [isEmail, "Proszę podać poprawny numer e-mail"],
+    },
+    password: {
+        type: String,
+        required: [true, "Proszę podać hasło"],
+        minlength: [6, "Hasło musi mieć co najmniej 6 znaków"],
+    },
+    family: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Family",
+    },
+});
+
+// encrypt password before saving to DB
+userSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+const Member = mongoose.model("member", memberSchema);
+
+module.exports = Member;
