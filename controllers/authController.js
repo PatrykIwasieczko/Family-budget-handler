6f4d1f5113043730944849e6900589f7fb2ab5a0
Patryk <patryk.iwasieczko@gmail.com>
2020-10-05 21:02:18 +0200

    Add signup route and controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,14 @@
 const Member = require("../models/Member");
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+// Load config
+dotenv.config({ path: "./config/config.env" });
 
 // create json web token
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
-    return jwt.sign({ id }, "secret", {
+    return jwt.sign({ id }, process.env.JWT_SECRET_TOKEN, {
         expiresIn: maxAge,
     });
 };
@@ -12,4 +16,8 @@ const createToken = (id) => {
 // controller actions
 module.exports.login_get = (req, res) => {
     res.render("login");
+};
+
+module.exports.signup_get = (req, res) => {
+    res.render("signup");
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authController");
 const router = Router();
 
 router.get("/login", authController.login_get);
+router.get("/signup", authController.signup_get);
 
 module.exports = router;
